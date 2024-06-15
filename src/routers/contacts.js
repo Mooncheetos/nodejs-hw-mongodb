a9b74567900bafff7299f3d200c075ab55f9281c
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-06-15 12:36:12 +0300

    firNex

---
+++ src/routers/contacts.js
@@ -0,0 +1,3 @@
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { Router } from 'express';
+import { createCo } from '../controllers/contacts.js';
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import mongoose from 'mongoose';
+
 import { env } from './utils/env.js';
 import { envVars } from './constants/envVars.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
