795724ee5d9683debdcbcbef72b57420ed2374d9
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-06-19 23:32:41 +0300

    therCo

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,17 +3,19 @@ import { env } from '../utils/env.js';
 import { envVars } from '../constants/envVars.js';
 
 export const initMongoConnection = async () => {
-  const connectionLink = `mongodb+srv://${env(envVars.MONGODB_USER)}:${env(
-    envVars.MONGODB_PASSWORD,
-  )}@${env(envVars.MONGODB_URL)}/${env(
-    envVars.MONGODB_DB,
-  )}?retryWrites=true&w=majority&appName=Cluster0`;
-
   try {
+    const user = env(envVars.MONGODB_USER);
+    const pwd = env(envVars.MONGODB_PASSWORD);
+    const url = env(envVars.MONGODB_URL);
+    const db = env(envVars.MONGODB_DB);
+
+    const connectionLink = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
     await mongoose.connect(connectionLink);
     console.log('Mongo connection successfully established!');
-  } catch (error) {
-    console.error(error.message);
-    throw error;
+  } catch (err) {
+    console.log(
+      'MongoDB connection error. Please make sure MongoDB is running.',
+    );
+    throw new Error(err);
   }
 };
