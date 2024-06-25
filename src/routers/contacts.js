307335d4ce01fc9eba6464266fe7b3203804291c
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-06-25 23:35:14 +0300

    firPagiSorto

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,13 +4,17 @@ import {
     createContactController, getContactByIdController, getContactsController,
     deleteContactController, putContactController, patchContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 
 const router = Router();
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.put('/contacts/:contactId', ctrlWrapper(putContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+router.put('/contacts/:contactId',validateBody(updateContactSchema), ctrlWrapper(putContactController));
+router.patch('/contacts/:contactId',validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,37 @@
 import { Contact } from '../db/models/contacts.js';
 import createHttpError from 'http-errors';
+import { SORT_ORDER } from '../constants/envVars.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+  const contactsCount = await Contact.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
 export const createContact = async (payload) => {
     return await Contact.create(payload);
-};
-export const getAllContacts = async () => {
-    return await Contact.find();
 };
 export const getContactById = async (id) => {
     return await Contact.findById(id);
