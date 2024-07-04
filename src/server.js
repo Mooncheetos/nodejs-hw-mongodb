a24b13c935b0ec899b650e0fe2a271502e45c66e
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-07-04 23:01:22 +0300

    zerUnoVer

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { env } from './utils/env.js';
 import { ENV_VARS, UPLOAD_DIR } from './constants/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 
@@ -27,6 +28,8 @@ export const setupServer = () => {
   app.use(cookieParser());
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
